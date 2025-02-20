3d4205d6d86ea761362b16ee6079808cffdc1cf3
Cassandra Choi <cass@cassc.dev>
2025-02-19 22:18:31 -0500

    feat: make textboxes only show the caret if focused

---
+++ modules/ui/textbox-focus.js
@@ -0,0 +1,6 @@
+import '/core/ui/components/fxs-textbox.js';
+
+const definition = Controls.getDefinition('fxs-textbox');
+definition.styles ??= [];
+definition.styles.push('fs://game/bobobunicorn-ui-tweaks/modules/ui/textbox-focus.css')
+
